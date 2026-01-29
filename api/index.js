e4b1374dba734db22714dde89de8f99e7ddca97a
krishnandwana <krishnandwana@gmail.com>
2026-01-29 12:44:41 +0530

    Fix Vercel configuration runtime error

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
-// api/index.js - Vercel serverless function entry point
 import { createRequestHandler } from "@remix-run/node";
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export default createRequestHandler({ 
+  build,
+  mode: process.env.NODE_ENV || "production"
+});
